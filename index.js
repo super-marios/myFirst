6a7740db59a26e99d178a0cd401e9273e538a80b
wootangg <wjckorea001@gmail.com>
2019-11-29 00:36:37 +0900

    added es6 http server

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ var wiki = require('./wiki.js');
 
 app.use('/wiki', wiki);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use(express.static('public'));
+
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500).send('Something broke!');
+});
 
 app.listen(3000, () => {
 	console.log('Example app listening on port 3000')
